53b2b4874e354785395c38aaec1e2cf7c8f4434e
Georg Sieber <it@georg-sieber.de>
2022-02-24 18:27:04 +0100

    avoid error if tree file is accessed directly

--- isc-dhcp-reservations/frontend/views/tree.d/isc-dhcp-reservations.php
+++ isc-dhcp-reservations/frontend/views/tree.d/isc-dhcp-reservations.php
@@ -1,4 +1,6 @@
+<?php if(empty($currentSystemUser)) die(); ?>
 <?php require_once(__DIR__.'/../../../lib/lib.d/isc-dhcp-reservations.php'); ?>
+
 <?php if($currentSystemUser->checkPermission(null, 'dhcp_reservation_management', false)) { ?>
 <div class='node'>
 	<a <?php echo explorerLink('views/views.d/isc-dhcp-reservations.php'); ?>><img src='img/img.d/dhcp.dyn.svg'>ISC DHCP Server Reservations</a>
