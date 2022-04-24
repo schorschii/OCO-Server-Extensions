95c17b722e225044735d3951af1776aa535b0be4
Georg Sieber <it@georg-sieber.de>
2022-04-24 13:49:14 +0200

    adjust sidebar

--- isc-dhcp-reservations/frontend/views/tree.d/isc-dhcp-reservations.php
+++ isc-dhcp-reservations/frontend/views/tree.d/isc-dhcp-reservations.php
@@ -4,7 +4,7 @@
 <?php if($currentSystemUser->checkPermission(null, 'dhcp_reservation_management', false)) { ?>
 <div id='divNodeIscDhcpReservations' class='node <?php if(count(ISC_DHCP_SERVER) > 1) echo "expandable"; ?>'>
 	<a <?php echo explorerLink('views/views.d/isc-dhcp-reservations.php'); ?>><img src='img/img.d/dhcp.dyn.svg'>ISC DHCP Server Reservations</a>
-	<div id='divNodeIscDhcpReservationsServers' class='subnode'>
+	<div id='divNodeIscDhcpReservationsServers' class='subitems'>
 		<?php
 		if(count(ISC_DHCP_SERVER) > 1) foreach(ISC_DHCP_SERVER as $server) {
 			$title = $server['TITLE'] ?? $server['ADDRESS'] ?? '???';
