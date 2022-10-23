e563552360e8e19c66bb3bd093397904f5e77dab
Georg Sieber <it@georg-sieber.de>
2022-10-23 16:28:14 +0200

    adjust table js

--- isc-dhcp-reservations/frontend/views/isc-dhcp-reservations.php
+++ isc-dhcp-reservations/frontend/views/isc-dhcp-reservations.php
@@ -43,68 +43,72 @@ try {
 	<button id='btnAddReservation' onclick='addIscDhcpReservation()' <?php if(!$permissionWrite) echo 'disabled'; ?>><img src='img/add.dyn.svg'>&nbsp;<?php echo LANG('add'); ?></button>
 </div>
 
-<table class="list searchable sortable savesort">
-
-	<thead>
-		<tr>
-			<th class='searchable sortable'><?php echo LANG('hostname'); ?></th>
-			<th class='searchable sortable'><?php echo LANG('internet_protocol_address'); ?></th>
-			<th class='searchable sortable'><?php echo LANG('media_access_control_address'); ?></th>
-			<th class=''><?php echo LANG('action'); ?></th>
-		</tr>
-	</thead>
-
-	<tbody>
-	<?php
+<div class='stickytable'>
+	<table class="list searchable sortable savesort">
+		<thead>
+			<tr>
+				<th class='searchable sortable'><?php echo LANG('hostname'); ?></th>
+				<th class='searchable sortable'><?php echo LANG('internet_protocol_address'); ?></th>
+				<th class='searchable sortable'><?php echo LANG('media_access_control_address'); ?></th>
+				<th class=''><?php echo LANG('action'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
 		$reservations = array();
 		if(!empty(trim($content))) {
 			$lines = preg_split("/((\r?\n)|(\r\n?))/", $content);
-
 			$hostname = '?'; $mac = '?'; $ip = '?';
 			foreach($lines as $line) {
-
-			// begin of host block -> truncate temporary values
-			if(substr($line,0,5) == "host ") {
-				$hostname = explode(" ", $line)[1];
-				$mac = '?'; $ip = '?';
-			}
-
-			// mac address definition
-			if(startsWith(trim($line), "hardware ethernet")) {
-				$mac = str_replace(";", "", explode(" ", trim($line))[2]);
-			}
-			// ip address definition
-			if(startsWith(trim($line), "fixed-address")) {
-				$ip = str_replace(";", "", explode(" ", trim($line))[1]);
-			}
-
-			// end of host block -> add host to result array
-			if(trim($line) == "}") {
-				$reservations[] = ['host'=>$hostname, 'mac'=>$mac, 'ip'=>$ip];
-				$hostname = '?'; $mac = '?'; $ip = '?';
+				// begin of host block -> truncate temporary values
+				if(substr($line,0,5) == "host ") {
+					$hostname = explode(" ", $line)[1];
+					$mac = '?'; $ip = '?';
+				}
+				// mac address definition
+				if(startsWith(trim($line), "hardware ethernet")) {
+					$mac = str_replace(";", "", explode(" ", trim($line))[2]);
+				}
+				// ip address definition
+				if(startsWith(trim($line), "fixed-address")) {
+					$ip = str_replace(";", "", explode(" ", trim($line))[1]);
+				}
+				// end of host block -> add host to result array
+				if(trim($line) == "}") {
+					$reservations[] = ['host'=>$hostname, 'mac'=>$mac, 'ip'=>$ip];
+					$hostname = '?'; $mac = '?'; $ip = '?';
+				}
 			}
 		}
-	}
-
-	foreach($reservations as $subresult) {
-		echo "<tr>\n";
-		echo "<td><span>".htmlspecialchars($subresult['host'])."</span></td>\n";
-		echo "<td><span>".htmlspecialchars($subresult['ip'])."</span></td>\n";
-		echo "<td><span>".htmlspecialchars($subresult['mac'])."</span></td>\n";
-		echo "<td>\n";
-		echo "<button class='small' onclick='removeIscDhcpReservation(\"".htmlspecialchars($subresult['host'],ENT_QUOTES)."\", \"".htmlspecialchars($subresult['ip'],ENT_QUOTES)."\", \"".htmlspecialchars($subresult['mac'],ENT_QUOTES)."\")' ".($permissionWrite?'':'disabled').">".LANG('delete')."</button>\n";
-		echo "</td>\n";
-		echo "</tr>\n\n";
-	}
-	?>
-	</tbody>
-
-	<tfoot>
-		<tr>
-			<td class='total' colspan='4'><span><?php echo count($reservations); ?> <?php echo LANG('reservations'); ?></span></td>
-		</tr>
-	</tfoot>
-</table>
+		foreach($reservations as $subresult) {
+			echo "<tr>\n";
+			echo "<td><span>".htmlspecialchars($subresult['host'])."</span></td>\n";
+			echo "<td><span>".htmlspecialchars($subresult['ip'])."</span></td>\n";
+			echo "<td><span>".htmlspecialchars($subresult['mac'])."</span></td>\n";
+			echo "<td>\n";
+			echo "<button class='small' onclick='removeIscDhcpReservation(\"".htmlspecialchars($subresult['host'],ENT_QUOTES)."\", \"".htmlspecialchars($subresult['ip'],ENT_QUOTES)."\", \"".htmlspecialchars($subresult['mac'],ENT_QUOTES)."\")' ".($permissionWrite?'':'disabled').">".LANG('delete')."</button>\n";
+			echo "</td>\n";
+			echo "</tr>\n\n";
+		}
+		?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<td class='total' colspan='4'>
+					<div class='spread'>
+						<div>
+							<span class='counterFiltered'>0</span>/<span class='counterTotal'>0</span>&nbsp;<?php echo LANG('reservations'); ?>,
+							<span class='counterSelected'>0</span>&nbsp;<?php echo LANG('selected'); ?>
+						</div>
+						<div class='controls'>
+							<button class='downloadCsv'><img src='img/csv.dyn.svg'>&nbsp;<?php echo LANG('csv'); ?></button>
+						</div>
+					</div>
+				</td>
+			</tr>
+		</tfoot>
+	</table>
+</div>
 
 <strings style='display:none'>
 	<string id='str_reservation_added_successfully'><?php echo LANG('reservation_added_successfully'); ?></string>
