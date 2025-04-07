99a904fe51214d0b7682c498f8da84e9d7516318
Georg Sieber <it@georg-sieber.de>
2025-04-07 16:16:16 +0200

    remove expandable class

--- paketeer/frontend/views/paketeer.tree.php
+++ paketeer/frontend/views/paketeer.tree.php
@@ -1,6 +1,6 @@
 <?php if(!empty($cl)) { ?>
 
-<div id='divNodePaketeer' class='node <?php if(count($servers) > 1) echo "expandable"; ?>'>
+<div id='divNodePaketeer' class='node'>
 	<a <?php echo explorerLink('views/paketeer.php'); ?>><img src='img/paketeer.dyn.svg'>Paketeer</a>
 </div>
 
